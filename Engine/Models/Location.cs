ed38f13f20c5582f549ffc310354e30bce5aeaf2
Alihan Baysal <alihan.baysal@intimetec.com>
2024-12-13 13:21:37 -0700

    feat: Added Icons for locations. Created Location Class. Updates GameSessions and MainWindows.xaml in order to support displaying current location and item.

---
+++ Engine/Models/Location.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Location
+    {
+        public int XCoordiante { get; set; }
+        public int YCoordiante { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageName { get; set; }
+    }
+}
--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -10,13 +10,27 @@ namespace Engine.ViewModels
     public class GameSession
     {
         public Player CurrentPlayer {  get; set; }
+        public Location CurrentLocation { get; set; }
 
+
+        // This is the constructor for the GameSession class
         public GameSession()
         {
             CurrentPlayer = new Player();
 
             CurrentPlayer.Name = "Alihan";
-            CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.Gold = 10;
+            CurrentPlayer.CharacterClass = "Barbarian";
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+            CurrentPlayer.HitPoints = 10;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordiante = 0;
+            CurrentLocation.YCoordiante = -1;
+            CurrentLocation.ImageName = "C:\\Users\\15417\\source\\repos\\WPFRPG\\Engine\\Images\\Locations\\TownSquare.png";
+            CurrentLocation.Description = "This is your home.";
         }
     }
 }
